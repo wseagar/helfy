f5b5fe826af1a53169add01f866f7bae461407d4
Walter Lim <waltissomewhere@gmail.com>
2023-08-19 13:45:04 +1200

    update content

--- src/app/components/completion.tsx
+++ src/app/components/completion.tsx
@@ -18,7 +18,7 @@ export function Completion() {
   });
 
   return (
-    <div className="bg-white p-8 rounded-xl shadow-md max-w-lg mx-auto mt-10">
+    <div className="bg-gray-900 text-white border border-gray-700 shadow-xl p-8 rounded-xl max-w-lg mx-auto mt-10">
       <h1 className="text-2xl font-bold mb-4">Recipe Generator</h1>
       <p className="mb-4">
         Type in any food you have in your fridge or pantry and our low carbon
@@ -29,7 +29,7 @@ export function Completion() {
           value={input}
           placeholder="Enter your ingredients here..."
           onChange={handleInputChange}
-          className="w-full p-2 border rounded-md placeholder-gray-400 focus:outline-none focus:border-blue-400"
+          className="w-full p-2 border border-gray-700 rounded-md placeholder-gray-300 focus:outline-none focus:border-blue-400 bg-gray-800 text-white"
         />
         <ReactMarkdown
           className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
@@ -45,22 +45,22 @@ export function Completion() {
         >
           {completion}
         </ReactMarkdown>
-        <div className="flex justify-end space-x-4">
-          <button
-            type="button"
-            onClick={stop}
-            className="py-2 px-4 bg-red-500 text-white rounded-md hover:bg-red-400 focus:outline-none"
-          >
-            Stop
-          </button>
+        <div className="flex justify-start space-x-4">
           <button
             disabled={isLoading}
             type="submit"
-            className={`py-2 px-4 bg-blue-500 text-white rounded-md ${
+            className={`py-2 px-4 bg-blue-500 text-white rounded-md flex-1 ${
               isLoading ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-400"
             } focus:outline-none`}
           >
             Submit
+          </button>
+          <button
+            type="button"
+            onClick={stop}
+            className="py-2 flex-1 px-4 bg-red-500 text-white rounded-md hover:bg-red-400 focus:outline-none"
+          >
+            Stop
           </button>
         </div>
       </form>
